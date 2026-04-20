08a16185ba85636c4957a7801a255b1751c0ff0c
alex <alexandre.peraldi@epita.fr>
2026-04-20 11:34:17 -0700

    First classes

---
+++ src/main/java/Transaction.java
@@ -0,0 +1,28 @@
+package src.main.java;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    private int id;
+    private int userId;
+    private double amount;
+    private String category;
+    private String description;
+    private LocalDate date;
+
+    public Transaction(int id, int userId, double amount, String category, String description, LocalDate date) {
+        this.id = id;
+        this.userId = userId;
+        this.amount = amount;
+        this.category = category;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() { return id; }
+    public int getUserId() { return userId; }
+    public double getAmount() { return amount; }
+    public String getCategory() { return category; }
+    public String getDescription() { return description; }
+    public LocalDate getDate() { return date; }
+}
