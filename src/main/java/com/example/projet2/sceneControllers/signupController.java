e281d45e000580f1a3403e013b057cebf380c659
Leo Prissberg <lprissberg@csumb.edu>
2026-05-01 10:45:30 -0700

    Built signup scene

--- src/main/java/com/example/projet2/sceneControllers/signupController.java
+++ src/main/java/com/example/projet2/sceneControllers/signupController.java
@@ -1,8 +1,12 @@
 package com.example.projet2.sceneControllers;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class signupController {
 
@@ -11,24 +15,43 @@ public class signupController {
 
     public Scene buildScene() {
         // Add in username field and text
+        Label username = new Label("Enter Username");
+        TextField usernameField = new TextField();
+        VBox usernameBox = new VBox(username,usernameField);
+
         // Add in password field and text
+        Label password = new Label("Enter Password");
+        PasswordField passwordField = new PasswordField();
+        VBox passwordBox = new VBox(password,passwordField);
+
         // Add in confirm field and text
+        Label confirm = new Label("Confirm Password");
+        PasswordField confirmField = new PasswordField();
+        VBox confirmBox = new VBox(confirm,confirmField);
 
         // Create signup button
+        Button signup = new Button("Sign up");
 
         // Create return to login button
+        Button rtrn = new Button("Return to login");
 
         // Store buttons in HBox
+        HBox buttonBox = new HBox(signup,rtrn);
 
         // Store components in VBox
+        VBox root = new VBox(usernameBox,passwordBox,confirmBox,buttonBox);
 
         // Create Scene with VBox as root
+        Scene scene = new Scene(root,SCENE_WIDTH,SCENE_HEIGHT);
 
         // Handle logic for signup button
+        signup.setOnAction(e -> {handleSignup(usernameField,passwordField,confirmField);});
 
         // Handle logic for return to login button
+        rtrn.setOnAction(e -> {handleReturn();});
 
         // Return Scene
+        return scene;
     }
 
     private void handleSignup(TextField usernameField, PasswordField passwordField, PasswordField confirmField) {
