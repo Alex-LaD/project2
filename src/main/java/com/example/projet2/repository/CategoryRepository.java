8478bde96df9d6e9ce56b1d2940670b1f184bb64
Gabriel <gabriel.simonian@epita.fr>
2026-05-01 17:28:26 -0700

    add CategoryRepository

---
+++ src/main/java/com/example/projet2/repository/CategoryRepository.java
@@ -0,0 +1,63 @@
+package com.example.projet2.repository;
+
+import com.example.projet2.Category;
+import com.example.projet2.database.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoryRepository {
+
+    public static void insertCategory(Category category) {
+        String sql = "INSERT INTO categories(name) VALUES(?)";
+
+        try (Connection conn = DatabaseManager.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, category.getName());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Category getCategoryByName(String name) {
+        String sql = "SELECT * FROM categories WHERE name = ?";
+
+        try (Connection conn = DatabaseManager.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, name);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return new Category(
+                        rs.getInt("id"),
+                        rs.getString("name")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void deleteCategoryByName(String name) {
+        String sql = "DELETE FROM categories WHERE name = ?";
+
+        try (Connection conn = DatabaseManager.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, name);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
